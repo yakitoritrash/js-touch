7840bc78762941a8551db86befbea24259bc71dc
Arch me <kishudwivedi30@gmail.com>
2025-09-17 08:02:04 +0530

    export esm

---
+++ export/one.js
@@ -0,0 +1,3 @@
+const greeting = "hello";
+const farewell = "bye";
+export { greeting, farewell };
---
+++ export/two.js
@@ -0,0 +1,4 @@
+import { greeting, farewell } from "./one.js"
+
+console.log(greeting);
+console.log(farewell);
