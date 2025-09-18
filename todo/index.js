1bc4c0220c4f0135a3ee0bfb9a3556643ee2e259
Arch me <kishudwivedi30@gmail.com>
2025-09-19 05:04:25 +0530

    display controller

---
+++ todo/index.js
@@ -0,0 +1,41 @@
+const todo = (title, description, due_date, priority) => {
+  return {
+    title, description, due_date, priority
+  }
+};
+
+const project = (name) => {
+  const project_todo = [];
+  return { name, project_todo };
+};
+
+const mainapp = (function () {
+  const project_arr = [];
+  const default_project = project("default");
+  project_arr.push(default_project);
+  const add_projects = function (name) {
+    const new_project = project(name);
+    project_arr.push(new_project);
+  }
+
+  const app_todo_to_project = function(project_name, tododetails) {
+    const targetproject = project_arr.find(project=> project.name === project_name);
+    if (targetproject) {
+      const new_todo = todo(
+        tododetails.title,
+        tododetails.description,
+        tododetails.due_date,
+        tododetails.priority
+      );
+      targetproject.project_todo.push(new_todo);
+    } else {
+      console.log(`Project ${project_name} not found.`)
+    }
+  }
+
+  return { add_projects, app_todo_to_project };
+})();
+
+const DisplayController = (function (mainapp) {
+
+})();
