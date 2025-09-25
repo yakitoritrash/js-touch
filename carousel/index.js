2c471e90fbab57dc9f5050d38294a3e5c969fee1
Arch me <kishudwivedi30@gmail.com>
2025-09-25 16:02:51 +0530

    done

--- carousel/index.js
+++ carousel/index.js
@@ -3,12 +3,17 @@ const left_btn = document.getElementById("left-control");
 const right_btn = document.getElementById("right-control");
 const dots = document.getElementById("dots");
 
+const dotElements = dots.children;
 const images = document.querySelectorAll(".carimage");
 const tot_slides = images.length;
 let currentSlide = 0;
 
 const updateCarousel = function() {
   filmstrip.style.transform = `translateX(${currentSlide * -100}%)`;
+  for (let i = 0; i < tot_slides; i++) {
+    dotElements[i].classList.remove("active-dot");
+  }
+  dotElements[currentSlide].classList.add("active-dot");
 }
 
 left_btn.addEventListener('click', () => {
@@ -26,3 +31,19 @@ right_btn.addEventListener('click', () => {
   }
   updateCarousel();
 })
+
+for (let i = 0; i < tot_slides; i++) {
+  let dote = document.createElement("div");
+  dots.appendChild(dote);
+  dote.addEventListener("click", () => {
+    currentSlide = i;
+    updateCarousel();
+  });
+};
+
+setInterval(() => {
+  right_btn.click();
+  if (currentSlide === tot_slides) {
+    left_btn.click();
+  }
+}, 5000);
