620bb359473fdcefb6de9b7b0bd7b1d2d4bc93a8
Arch me <kishudwivedi30@gmail.com>
2025-09-30 14:10:16 +0530

    api call

--- weather-app/index.js
+++ weather-app/index.js
@@ -1,3 +1,17 @@
 async function getWeatherData(location) {
-  const response = fetch();
+  const apiKey = "6f60690035e9aa02c004a049798f430b";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+  try {
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.log("error fetching weather data:", error);
+  }
+}
+
+const apiData = getWeatherData("delhi");
+
+function processData(apiData) {
+
 }
