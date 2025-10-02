592254729e4d1e01bdc8bd69dfd594fe9ba92005
Arch me <kishudwivedi30@gmail.com>
2025-10-02 10:41:29 +0530

    product of an array

--- recursion/quiz.js
+++ recursion/quiz.js
@@ -23,5 +23,24 @@ function factorial(x) {
   }
 }
 
+const allAreLessThanSeven = all([1, 2, 9], function(num) {
+  return num < 7;
+});
+
+function all(arr, callback)  {
+  var copy = copy || arr.slice();
+  if (copy.length === 0) return true;
+  if (callback(copy[0])) {
+    copy.shift();
+    return all(copy, callback);
+  } else {
+    return false;
+  }
+}
+
+function productOfarray (arr) {
+
+}
+
 const output = factorial(5);
-console.log(output);
+console.log(allAreLessThanSeven);
