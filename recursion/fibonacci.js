2c9e18ecbece02ebea4f74e3ec3ff93fa1ad86bf
Arch me <kishudwivedi30@gmail.com>
2025-10-02 15:44:08 +0530

    fib

---
+++ recursion/fibonacci.js
@@ -0,0 +1,14 @@
+function fibonacci(num) {
+  let arr = [];
+  if (num === 0) {
+    arr.push(0);
+  } else if (num === 1) {
+    arr.push(1);
+  } else {
+    arr.push(num + fibonacci(num - 1) + fibonacci(num - 2));
+  }
+  return arr
+}
+
+const output = fibonacci(8)
+console.log(output);
